0b9e521e082512ed908da1420ecd5e7b4afc3770
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-22 12:46:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -958,7 +958,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.IMAGE_TXT.format(temp.B_NAME),
+            text=Script.IMAGE_TXT.format(temp.B_NAME),
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode='html'
