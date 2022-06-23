d935f24046125781b0ada8c4cb1076f4a4a53cac
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-06-23 20:48:26 +0530

    Update __init__.py

--- wbb/__init__.py
+++ wbb/__init__.py
@@ -1,8 +1,34 @@
-from pyrogram import Client 
+from pyrogram import Client as app
 import os
-
+import time
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from info import DATABASE_URI
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
 bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
+class Log:
+    def __init__(self, save_to_file=False, file_name="wbb.log"):
+        self.save_to_file = save_to_file
+        self.file_name = file_name
+
+    def info(self, msg):
+        print(f"[+]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[INFO]({time.ctime(time.time())}): {msg}\n")
+
+    def error(self, msg):
+        print(f"[-]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[ERROR]({time.ctime(time.time())}): {msg}\n")
+
+
+log = Log(True, "bot.log")
+
+# MongoDB client
+log.info("Initializing MongoDB client")
+mongo_client = MongoClient(DATABASE_URI)
+db = mongo_client.wbb
