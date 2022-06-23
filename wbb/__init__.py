1f506246fc4508cc610098b821df33265fd45cbb
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-06-23 20:55:46 +0530

    Update __init__.py

--- wbb/__init__.py
+++ wbb/__init__.py
@@ -7,7 +7,6 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
-bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
 class Log:
     def __init__(self, save_to_file=False, file_name="wbb.log"):
         self.save_to_file = save_to_file
